bd48728074d68455ec1558902413e46bb1479744
Sergey Khegay <g.sergeykhegay@gmail.com>
2017-04-05 00:48:39 -0400

    Fix import issue

--- src/main/java/edu/nyu/cs9053/homework8/LambdaJob.java
+++ src/main/java/edu/nyu/cs9053/homework8/LambdaJob.java
@@ -1,7 +1,5 @@
 package edu.nyu.cs9053.homework8;
 
-import edu.nyu.cs9053.homework8.base.AbstractJob;
-
 public class LambdaJob extends AbstractJob {
 
     public LambdaJob(long startTime, long finishTime) {
